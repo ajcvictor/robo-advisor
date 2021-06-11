50b68e6928b42de6d7c8a2c664982fa7e5a553df
ajcvictor <ajv269@stern.nyu.edu>
2021-06-10 20:15:49 -0400

    Updated Readme for setup

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # robo-advisor
+
+
+##APIKEY: KDAKV7SOOZHAWOBM
+
+
+#link: https://www.alphavantage.co/query?function=CRYPTO_RATING&symbol=ETH&apikey=KDAKV7SOOZHAWOBM
+
+
+#URL to request data from: https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=KDAKV7SOOZHAWOBM
+
+
+
+import os
+import dotenv import load_dotenv
+import requests
+
+
+#red env variables
+
+load_dotenv()       # 
+
+
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")     
+
+
+ # read frm .env file
+
+
+
+
+
+#make a request
+
+import requests
+
+symbol = "MSFT"   # ask for user input instead of hardcoding
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+response = requests.et(request_url)
+print(type(response))
+print(response.text)
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,1 +1,46 @@
 print("TODO: make a web request for some stock")
+
+
+
+#import os
+##import dotenv import load_dotenv
+import requests
+
+
+#red env variables
+
+#load_dotenv()       # 
+
+
+##ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")     
+
+
+ # read frm .env file
+
+
+
+#make a request
+
+#import requests
+
+#symbol = "MSFT"   # ask for user input instead of hardcoding
+
+#request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+#response = requests.et(request_url)
+#print(type(response))
+##print(response.text)
+
+
+import os
+from dotenv import load_dotenv
+import requests
+load_dotenv() # go get env vars from the .env file
+# read env variables
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+# make a request
+symbol = "MSFT" # ask for a user input
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+response = requests.get(request_url)
+print(type(response))
+print(response.text)
